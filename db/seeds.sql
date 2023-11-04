3283cd0171478055da70633311e097e8682d2aa7
Hadiqa <hadiqa.aziz@gmail.com>
2023-11-04 08:37:22 -0700

    include files

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+USE company;
+INSERT INTO departments (name)
+VALUES
+ ("Sales"),
+ ("Engineering"),
+ ("Finance"),
+ ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000, 1), 
+("Salesperson", 100000, 1), 
+("Lead Engineer", 150000, 2), 
+("Software Engineer", 120000, 2), 
+("Account Manager", 100000, 3), 
+("Legal Team Lead", 300000, 4), 
+("Lawyer", 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES 
+("John", "Doe", 1), 
+("Mike", "Chan", 2), 
+("Ashley", "Rodriguez", 3),
+("Kevin", "Tupik", 4), 
+("Kunal", "Singh", 5), 
+("Malia", "Brown", 6), 
+("Sarah", "Lourd", 7), 
+("Kurt", "Vonnegut", 5), 
+("Tom", "Allen", 4);
